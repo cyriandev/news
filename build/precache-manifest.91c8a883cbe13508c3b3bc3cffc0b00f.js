392e912c7a26274b8d620ebc947235fbf98047ad
cyriandev <cyriandev@gmail.com>
2022-11-24 10:36:04 +0200

    ran the built script

---
+++ build/precache-manifest.91c8a883cbe13508c3b3bc3cffc0b00f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eeb0798e5d4a4ab361bf8ba00873769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc7b3f4a5ab2740551c",
+    "url": "/static/css/main.7e2f5872.chunk.css"
+  },
+  {
+    "revision": "099a0b84e0f734dba0d4",
+    "url": "/static/js/2.eddf5cae.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.eddf5cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc7b3f4a5ab2740551c",
+    "url": "/static/js/main.73cff3e4.chunk.js"
+  },
+  {
+    "revision": "82af36e83412180715d7",
+    "url": "/static/js/runtime-main.86c19a1b.js"
+  },
+  {
+    "revision": "d32a6bcd8130b78d4771a1f0d1ab0c4e",
+    "url": "/static/media/n-logo.d32a6bcd.png"
+  }
+]);
