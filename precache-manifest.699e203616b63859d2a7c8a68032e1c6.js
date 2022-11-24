f964f2743165e0ccfb4a0cc3d2f046f6d04f2834
cyriandev <cyriandev@gmail.com>
2022-11-24 10:44:44 +0200

    Updates

---
+++ precache-manifest.699e203616b63859d2a7c8a68032e1c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e639c3a3312c147e4f146baadf218a09",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9e0d29366566e14341d0",
+    "url": "/react-gh-pages/static/css/main.7e2f5872.chunk.css"
+  },
+  {
+    "revision": "099a0b84e0f734dba0d4",
+    "url": "/react-gh-pages/static/js/2.eddf5cae.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/react-gh-pages/static/js/2.eddf5cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0d29366566e14341d0",
+    "url": "/react-gh-pages/static/js/main.dec3f4f1.chunk.js"
+  },
+  {
+    "revision": "77607358f4407843ab81",
+    "url": "/react-gh-pages/static/js/runtime-main.5d6d6194.js"
+  },
+  {
+    "revision": "d32a6bcd8130b78d4771a1f0d1ab0c4e",
+    "url": "/react-gh-pages/static/media/n-logo.d32a6bcd.png"
+  }
+]);
